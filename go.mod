668e52352912df565d8ff3232cde3cb7d288d540
langxw1 <langxianzhe@gmail.com>
2020-10-16 15:18:03 +0800

    实现了读取 JSON 文件并写入 yaml 文件

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Jasson/meerkat-convert
+
+go 1.12
+
+require (
+	github.com/jinzhu/configor v1.2.0
+	gopkg.in/yaml.v2 v2.2.2
+)
---
+++ mc_yaml/write_yaml.go
@@ -0,0 +1,42 @@
+package mc_yaml
+
+import (
+	"gopkg.in/yaml.v2"
+	"log"
+	"os"
+)
+
+func init() {
+	log.SetFlags(log.Ldate | log.Lshortfile)
+
+}
+
+var data = `
+a: Easy!
+b:
+  c: 2
+  d: [3, 4]
+`
+
+// Note: struct fields must be public in order for unmarshal to
+// correctly populate the data.
+type T struct {
+	A string
+	B struct {
+		RenamedC int   `yaml:"c"`
+		D        []int `yaml:",flow"`
+	}
+}
+
+func Write(writeContext interface{} ) {
+	d, err := yaml.Marshal(&writeContext)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	log.Printf("--- t dump:\n%s\n\n", string(d))
+
+	file, err := os.OpenFile("new.yaml", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file.Write(d)
+	defer file.Close()
+
+}
